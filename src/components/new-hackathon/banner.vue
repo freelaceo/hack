768f972267ad6855b08ca58a343d6d8d037e2ea8
Luis Vilches <luisvilches@Luis-Vilches-MacBook-Pro.local>
2018-04-13 10:09:43 -0300

    first commit

---
+++ src/components/new-hackathon/banner.vue
@@ -0,0 +1,67 @@
+<template>
+  	<section class="section">
+        <div class="banner" v-bind:style="{ backgroundImage: 'url(' + img + ')' }">
+            <div v-if="type === 'success'">
+                <a @click="event" class="btn btn-lg btn-green mr-15 mb-20">{{val}}</a>
+            </div>
+            <div v-else-if="type === 'primary'">
+                <a @click="event" class="btn btn-lg btn-blue mr-15 mb-20">{{val}}</a>
+            </div>
+
+            <div v-else>
+                <a @click="event" class="btn btn-lg mr-15 mb-20">{{val}}</a>
+            </div>
+            <a class="btn btn-xs btn-black mr-15 mb-20">...</a>
+        </div>
+        <div v-if="creator" class="options">
+            <ul>
+                <li><a href="">Edit</a></li>
+                <li><a href="">Save Changes</a></li>
+            </ul>
+        </div>
+        <div class="space"> </div>
+    </section>
+</template>
+
+<style scoped>
+    .banner {
+        width: 100%;
+        height: 330px;
+        overflow: hidden;
+        background-position: center;
+        display: flex;
+        align-items: flex-end;
+        justify-content: flex-end;
+        margin-top: 80px;
+    }
+
+    .space{
+        margin-bottom: 30px!important;
+    }
+
+    .options{
+        height: 40px;
+        background: #4A4A4A;
+        padding: 0px 40px;
+    }
+
+    .options ul li {
+        display: inline-block;
+        list-style: none;
+        color: #fff;
+        font-size: 18px;
+        margin: 10px 20px;
+    }
+</style>
+
+<script>
+export default {
+  name:"banner",
+  props:['val','type','img','event'],
+  data(){
+      return{
+          creator:true
+      }
+  }
+}
+</script>
---
+++ src/store/modules/auth.js
@@ -0,0 +1,106 @@
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import * as types from '../mutation-types'
+import Router from 'vue-router'
+
+// state
+export const state = {
+  user: Cookies.get('user') || null,
+  token: Cookies.get('token')
+}
+
+// getters
+export const getters = {
+  user: state => state.user,
+  token: state => state.token,
+  check: state => state.user !== null
+}
+
+// mutations
+export const mutations = {
+  [types.SAVE_TOKEN] (state, { token,user, remember }) {
+    console.log("token =>",token);
+    state.token = token;
+    state.user = user;
+    Cookies.set('user',user,{expires:remember ? 365 : null})
+    Cookies.set('token', token, { expires: remember ? 365 : null })
+  },
+
+  [types.FETCH_USER_SUCCESS] (state, { user }) {
+    console.log(user)
+    state.user = user
+  },
+
+  [types.FETCH_USER_FAILURE] (state) {
+    state.token = null
+    Cookies.remove('token')
+  },
+
+  [types.LOGOUT] (state) {
+    state.user = null
+    state.token = null
+
+    Cookies.remove('token')
+    Cookies.remove('user')
+  },
+
+  [types.UPDATE_USER] (state, { user }) {
+    state.user = user
+  },
+
+  [types.CREATE_HACKATHON] (state, { obj }) {
+    console.log(obj);
+  }
+}
+
+// actions
+export const actions = {
+  saveToken ({ commit, dispatch }, payload) {
+    commit(types.SAVE_TOKEN, payload)
+  },
+
+  async fetchUser ({ commit }) {
+    console.log("se activa")
+    try {
+      const { data } = await axios.get('/auth/user/' + state.user._id)
+      commit(types.FETCH_USER_SUCCESS, { user: data })
+    } catch (e) {
+      commit(types.FETCH_USER_FAILURE)
+    }
+  },
+
+  updateUser ({ commit }, payload) {
+    commit(types.UPDATE_USER, payload)
+  },
+
+  async logout ({ commit }) {
+    commit(types.LOGOUT)
+  },
+
+  async fetchOauthUrl (ctx, { provider }) {
+    const { data } = await axios.post(`http://api.hackathon.gravityweb.com.ve:8003/api/oauth/${provider}`)
+    return data.url
+  },
+
+  async createHackathon ({commit},p){
+    var userID = JSON.parse(state.user);
+    var f = new FormData();
+        f.append('place',p.form.place);
+        f.append('type',p.form.type);
+        f.append('invite',p.form.invite);
+        f.append('title',p.form.title);
+        f.append('address',p.form.address);
+        f.append('linkreserv',p.form.linkreserv);
+        f.append('overvies',p.form.overvies);
+        f.append('shedule',p.form.schedule);
+        f.append('date',p.form.date);
+        f.append('userId',userID._id);
+    
+    var data = await axios('/auth/hackathon',{
+        method:"post",
+        data: f
+    })
+    console.log(data.data.data)
+    p.$router.push({name: 'home' , params: { id: data.data.data.titleLink }});
+  }
+}
